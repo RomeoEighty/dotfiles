8d5cfbcbe712395f237bb81675a60ddf9e2043ad
RomeoEighty <major.yamaguchi@gmail.com>
2018-04-13 19:37:52 +0900

    bug fix

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ do
     fi
 
     #echo "${SOURCEFILEABSPATH}" "${HOME}/${f}"
-    ln -snfv "${f}" "${HOME}"/"${f}"
+    ln -snfv "${SOURCEFILEABSPATH}" "${HOME}"/"${f}"
 done
 
 # For debug, print all variables
